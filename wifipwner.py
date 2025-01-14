0e824cfffccfd49ed43c78c009975040481bfd4d
Cyber-JA <giuseppecaruso0990@gmail.com>
2025-01-14 12:34:33 -0500

    Minor changes

--- wifipwner.py
+++ wifipwner.py
@@ -5,6 +5,10 @@ import textwrap
 import subprocess
 import re
 
+# Handle user CTRL+C
+def signal_handler(sig, frame):
+    print("[-] CTRL+C detected.")
+
 def manage_interface(interface):
     command = "iwconfig " + interface
     try:
@@ -31,29 +35,34 @@ if __name__ == '__main__':
         description='Auto deauthenticator tool using aircrack-ng suite',
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent('''Example:
-    deauthenticator.py -b 0A:1B:2C:3D:4E:5F -t 0A:1B:2C:3D:4E:5F -c 10 -i wlan1mon
+    wifipwner.py -b 0A:1B:2C:3D:4E:5F -t 0A:1B:2C:3D:4E:5F -c 10 -a bruter -i wlan1mon
     ''')
     )
     parser.add_argument('-t', '--target', help='MAC of the target to deauthenticate (0 to broadcast)')
     parser.add_argument('-b', '--bssid', help='MAC address of the AP')
     parser.add_argument('-c', '--counter', help='Number of deauth requests to be sent', type=int)
     parser.add_argument('-i', '--interface', help='Network interface to be used')
+    parser.add_argument('-a', '--attack', help='Attack mode to be used (bruter, dictionary)', type=str)
     args = parser.parse_args()
 
-    if args.target == None:
-        print("[!] Missing Target...Aborting")
-    elif args.bssid == None:
-        print("[!] Missing BSSID...Aborting")
-    elif args.counter == None:
-        print("[!] Missing Counter...Aborting")
-    elif args.interface == None:
-        print("[!] Missing Interface...Aborting")
-    else:
-        target = args.target
-        bssid = args.bssid
-        counter = args.counter
-        interface = args.interface
-        manage_interface(interface=interface)
-        check_MAC_format(bssid)
-        check_MAC_format(target)
+    try:
+        if args.target == None or check_MAC_format(args.target) == False:
+            print("[!] Missing or wrong Target format...Aborting")
+        elif args.bssid == None or check_MAC_format(args.bssid) == False:
+            print("[!] Missing or wrong BSSID format...Aborting")
+        elif args.counter == None:
+            print("[!] Missing Counter...Aborting")
+        elif args.interface == None:
+            print("[!] Missing Interface...Aborting")
+        elif args.attack == None:
+            print("[!] Missing Attack mode...Aborting")
+        else:
+            target = args.target
+            bssid = args.bssid
+            counter = args.counter
+            interface = args.interface
+            attack = args.attack
+            manage_interface(interface=interface)
+    except KeyboardInterrupt:
+
 
